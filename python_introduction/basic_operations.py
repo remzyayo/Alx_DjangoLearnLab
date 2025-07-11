f0b179f369c43f91d94fdcfd01fd34d731c553b3
Francis <oluremifrancisaya@gmailcom>
2025-07-11 12:17:30 -0700

    Initial Commit

--- python_introduction/basic_operations.py
+++ python_introduction/basic_operations.py
@@ -11,4 +11,3 @@ multiplication = number1 * number2
 print(f"Addition of {number1} and {number2} is {addition}")
 print(f"Subtraction of {number1} and {number2} is {subtraction}")
 print(f"Multiplication of {number1} and {number2} is {multiplication}")
-

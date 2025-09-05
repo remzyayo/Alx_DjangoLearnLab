5bb4eec17d7248c7cbf54c2d102568698b8ab1da
Francis <oluremifrancisaya@gmailcom>
2025-09-05 12:23:12 -0700

    updated

---
+++ Alx_DjangoLearnLab/django-models/models.py
@@ -0,0 +1,31 @@
+from django.db import models
+
+class Author(models.Model):
+    name = models.CharField(max_length=100)
+
+    def _str_(self):
+        return self.name
+
+
+class Book(models.Model):
+    title = models.CharField(max_length=200)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+
+    def _str_(self):
+        return self.title
+
+
+class Library(models.Model):
+    name = models.CharField(max_length=150)
+    books = models.ManyToManyField(Book)
+
+    def _str_(self):
+        return self.name
+
+
+class Librarian(models.Model):
+    name = models.CharField(max_length=100)
+    library = models.OneToOneField(Library, on_delete=models.CASCADE)
+
+    def _str_(self):
+        return self.name

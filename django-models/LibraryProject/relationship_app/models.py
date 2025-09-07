fbece5b6e055f7003a907172a57f4e788ebc491e
REMZY <oluremifrancisaya@gmail.com>
2025-09-07 12:44:18 -0700

    Update models.py

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -9,7 +9,7 @@ from django.dispatch import receiver
 class Author(models.Model):
     name = models.CharField(max_length=100)
 
-    def _str_(self):
+    def __str__(self):
         return self.name
 
 
@@ -17,7 +17,7 @@ class Book(models.Model):
     title = models.CharField(max_length=200)
     author = models.ForeignKey(Author, on_delete=models.CASCADE, related_name="books")
 
-    def _str_(self):
+    def __str__(self):
         return self.title
 
 
@@ -25,7 +25,7 @@ class Library(models.Model):
     name = models.CharField(max_length=100)
     books = models.ManyToManyField(Book, related_name="libraries")
 
-    def _str_(self):
+    def __str__(self):
         return self.name
 
 
@@ -33,7 +33,7 @@ class Librarian(models.Model):
     name = models.CharField(max_length=100)
     library = models.OneToOneField(Library, on_delete=models.CASCADE, related_name="librarian")
 
-    def _str_(self):
+    def __str__(self):
         return self.name
 
 
@@ -50,7 +50,7 @@ class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="profile")
     role = models.CharField(max_length=20, choices=ROLE_CHOICES, default="Member")
 
-    def _str_(self):
+    def __str__(self):
         return f"{self.user.username} - {self.role}"
 
 

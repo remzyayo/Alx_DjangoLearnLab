fb9326eab1804f01d764d64292bb255e00e31bb6
REMZY <oluremifrancisaya@gmail.com>
2025-09-07 05:08:45 -0700

    Update urls.py

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
-from .views import list_books
+from . import views
+from .views import list_books, LibraryDetailView
+from .views import register_view, login_view, logout_view
 
 urlpatterns = [
     # Function-based view
@@ -7,4 +9,9 @@ urlpatterns = [
 
     # Class-based view
     path("library/<int:pk>/", views.LibraryDetailView.as_view(), name="library_detail"),
+
+    # Authentication views
+    path("register/", register_view, name="register"),
+    path("login/", login_view, name="login"),
+    path("logout/", logout_view, name="logout"),
 ]

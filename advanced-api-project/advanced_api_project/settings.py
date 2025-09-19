402e7a46792b9fa4e5f521d2b20a726260cab673
Francis <oluremifrancisaya@gmailcom>
2025-09-19 04:27:38 -0700

    Added filtering, searching, and ordering

--- advanced-api-project/advanced_api_project/settings.py
+++ advanced-api-project/advanced_api_project/settings.py
@@ -50,6 +50,14 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
+REST_FRAMEWORK = {
+    'DEFAULT_FILTER_BACKENDS': [
+        'django_filters.rest_framework.DjangoFilterBackend',
+        'rest_framework.filters.SearchFilter',
+        'rest_framework.filters.OrderingFilter',
+    ]
+}
+
 ROOT_URLCONF = 'advanced_api_project.urls'
 
 TEMPLATES = [
--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -5,6 +5,7 @@ from rest_framework.response import Response
 from django.shortcuts import get_object_or_404
 from .models import Book
 from .serializers import BookSerializer
+from django_filters.rest_framework import DjangoFilterBackend
 
 # Create your views here.
 

ed95524a08cacdf945468658db9d0f0b75474f74
Francis <oluremifrancisaya@gmailcom>
2025-09-30 09:12:12 +0100

    Add user authentication: regiser/login/logout/profile + profilemodel

--- django_blog/django_blog/settings.py
+++ django_blog/django_blog/settings.py
@@ -122,6 +122,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'blog/static')]
+STATIC_ROOT = BASE_DIR / "staticfiles"
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 

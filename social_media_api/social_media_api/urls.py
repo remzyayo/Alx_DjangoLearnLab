2b600e506ba9e52127d6971305a889e2ac2062bb
Francis <oluremifrancisaya@gmailcom>
2025-10-08 20:31:54 +0100

    Welcome to Social Media API

--- social_media_api/social_media_api/urls.py
+++ social_media_api/social_media_api/urls.py
@@ -19,13 +19,14 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.http import HttpResponse
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/accounts/', include('accounts.urls')),
-    path('api/accounts/', include('posts.urls')),
     path('api/posts/', include('posts.urls', namespace='posts')),
-    path('api/notifications/', include('notification.urls', namespace='notifications')),
+    path('api/notifications/', include('notifications.urls', namespace='notifications')),
+    path('', lambda request: HttpResponse("Welcome so the social Media API!")),
 ]
 
 if settings.DEBUG:

31d3f1c9ced2dd19d39ea36d9adb2e0b497d1bb8
Francis <oluremifrancisaya@gmailcom>
2025-10-09 07:45:17 +0100

    Welcome to Social Medial API!

--- social_media_api/social_media_api/settings.py
+++ social_media_api/social_media_api/settings.py
@@ -22,12 +22,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ouw22waynkuv^&m(539v+944cp*c06x+vx&3i#&g@7f4^%qm8j'
+SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-ouw22waynkuv^&m(539v+944cp*c06x+vx&3i#&g@7f4^%qm8j')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['.onrender.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
